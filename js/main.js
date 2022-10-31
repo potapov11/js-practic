9cc6b8b5b3caae1da616a6348498a4f4ab5872e8
Serezha <volos11223344@yandex.ru>
2022-10-31 22:19:22 +0300

    start new project - home work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+
+  },
+  actors: {
+
+  },
+  genres: [],
+  privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько вы оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько вы оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
