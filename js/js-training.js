07a8e263307b3424c5d0f6bc99dbaa2e190bcc85
Serezha <volos11223344@yandex.ru>
2022-11-02 21:49:14 +0300

    logical operator

--- js/js-training.js
+++ js/js-training.js
@@ -1,15 +1,35 @@
-const num = '50';
+// const hamburger = true;
+// const fries = false;
 
-switch (num) {
-  case '49':
-    console.log('неверно');
-    break;
-  case '100':
-    console.log('Неверно');
-    break;
-  case '50':
-    console.log('В точку');
-    break;  
-  default: 
-    console.log('Не в этот раз');
-};
+// if(hamburger && fries) {
+//   console.log('Я сыт');
+// }
+
+// console.log((hamburger && fries));
+
+// В JS есть пять сущностей которые всегда false:
+
+// 1. null
+// 2. Undefined
+// 3. 0
+// 4. ''  - пустая строка
+// 5. NaN
+
+const hamburger = 3;
+const fries = 1;
+const cola = 0;
+
+console.log(hamburger === 3 && fries && cola);
+
+if (hamburger === 3 && fries === 1 && cola === 1) {
+  console.log('Мы сыты');
+} else {
+  console.log('Идем в другое место');
+}
+
+const StringUser = 'kbkbkbkjbkjb';
+const StringUser2 = 'zxczxczxxzccx';
+
+console.log(StringUser && StringUser2);
+
+
