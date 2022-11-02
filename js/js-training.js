f5142c354ab096316fc6440b3c244f734f8207da
Serezha <volos11223344@yandex.ru>
2022-11-02 19:15:04 +0300

    switch condition

---
+++ js/js-training.js
@@ -0,0 +1,15 @@
+const num = '50';
+
+switch (num) {
+  case '49':
+    console.log('неверно');
+    break;
+  case '100':
+    console.log('Неверно');
+    break;
+  case '50':
+    console.log('В точку');
+    break;  
+  default: 
+    console.log('Не в этот раз');
+};
