3bd0b2ef4b8fc84b954ce14b2ee7c944961f2ba9
Serezha <volos11223344@yandex.ru>
2022-11-20 16:33:15 +0300

    tasks with objects

--- js/js-training.js
+++ js/js-training.js
@@ -1,24 +1,46 @@
-function first() {
-  // do something
-  setTimeout(function() {
-    console.log(1);
-  },500); 
-}
-
-function second() {
-  console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-  console.log(`я учу ${lang}`);
-  callback();
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'balck',
+    background: 'red',
+  }
 };
 
-function done() {
-  console.log('Я прошел этот урок');
+console.log(options['colors']['border']);
+
+let counter=0;
+for (let key in options) {
+  if(typeof(options[key]) === 'object') {
+    for(let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+      counter++;
+    }
+  } else{
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+  }
+}
+
+console.log(counter);
+
+
+// let counter = 0;
+
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'balck',
+    background: 'red',
+  }
 };
 
-learnJS('jS', done);
+// for (let key in options) {
+//   counter++;
+// }
+// console.log(counter);
+
+console.log(Object.keys(options));
