5e605787a0ef11ba8c0fb7d1b13dc5bbae5dc4d9
Serezha <volos11223344@yandex.ru>
2022-11-20 14:42:08 +0300

    callback

--- js/js-training.js
+++ js/js-training.js
@@ -1,29 +1,24 @@
-function sayHello (a) {
-  console.log(a);
+function first() {
+  // do something
+  setTimeout(function() {
+    console.log(1);
+  },500); 
 }
 
-sayHello();
-
-function cub(cubLength) {
-  if(!Number.isInteger(cubLength) || typeof cubLength === 'string' || cubLength <= 0) {
-    return 'При вычислении произошла ошибка';
-  } else {
-  return `Объем куба: ${cubLength*cubLength*6}, площадь всей поверхности: ${cubLength*cubLength*cubLength}`;}
-}
-console.log(cub(80));
-
-
-function getCoupeNumber(seatNumber) {
-  if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
-      return "Ошибка. Проверьте правильность введенного номера места";
-  }
-
-  if (seatNumber === 0 || seatNumber > 36) {
-      return "Таких мест в вагоне не существует";
-  }
-
-  return Math.ceil(seatNumber / 4);
-  // тут очень много вариантов решения, но выбрал один из элегантных :)
+function second() {
+  console.log(2);
 }
 
-console.log(getCoupeNumber(3));
+first();
+second();
+
+function learnJS(lang, callback) {
+  console.log(`я учу ${lang}`);
+  callback();
+};
+
+function done() {
+  console.log('Я прошел этот урок');
+};
+
+learnJS('jS', done);
