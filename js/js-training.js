8497503a517bcadde1a870b4033ba93f10166e4b
Serezha <volos11223344@yandex.ru>
2022-11-21 19:24:20 +0300

    objects

--- js/js-training.js
+++ js/js-training.js
@@ -5,42 +5,15 @@ const options = {
   colors: {
     border: 'balck',
     background: 'red',
+  },
+  makeTest: function() {
+    console.log('Test');
   }
 };
 
+options.makeTest();
+
+
+console.log(options.name )('Проверка через точку');
+console.log(options['width']) ('Проверка через скобку');
 console.log(options['colors']['border']);
-
-let counter=0;
-for (let key in options) {
-  if(typeof(options[key]) === 'object') {
-    for(let i in options[key]) {
-      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-      counter++;
-    }
-  } else{
-    console.log(`Свойство ${key} имеет значение ${options[key]}`);
-    counter++;
-  }
-}
-
-console.log(counter);
-
-
-// let counter = 0;
-
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'balck',
-    background: 'red',
-  }
-};
-
-// for (let key in options) {
-//   counter++;
-// }
-// console.log(counter);
-
-console.log(Object.keys(options));
