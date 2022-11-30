a2ed72d5ecdd7fdb5d6fe1e3f59d8f1aecff5adc
Serezha <volos11223344@yandex.ru>
2022-11-30 16:17:54 +0300

    oj Copy tasks

--- js/js-training.js
+++ js/js-training.js
@@ -14,6 +14,52 @@ const options = {
 options.makeTest();
 
 
-console.log(options.name )('Проверка через точку');
-console.log(options['width']) ('Проверка через скобку');
-console.log(options['colors']['border']);
+// console.log(options.name )('Проверка через точку');
+// console.log(options['width']) ('Проверка через скобку');
+// console.log(options['colors']['border']);
+
+// function copy(mainObj) {
+//   let objCopy = {};
+
+//   let key;
+
+//   for (key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
+
+//   return objCopy;
+// };
+
+
+// const numbers = {
+//   a: 2,
+//   b: 3,
+//   c: {
+//     x: 7,
+//     y: 12,
+//   }
+// };
+
+// const newNumbers = copy(numbers);
+// console.log(newNumbers);
+
+function copyObj (mainObj) {
+  let newObj = {};
+
+  let key;
+
+  for(key in mainObj) {
+    newObj[key] = mainObj[key];
+  }
+  return newObj;
+}
+
+const namesObj = {
+  a: 'John',
+  b: 'Altun',
+  c: 'Nikolai',
+};
+
+const newNamesObj = copyObj(namesObj);
+
+console.log(namesObj);
