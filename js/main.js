8ec88a933c5abccb6960ae29bfbec6da6fd4566f
Serezha <volos11223344@yandex.ru>
2022-12-08 10:08:37 +0300

    js tst

--- js/main.js
+++ js/main.js
@@ -1,23 +1,5 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// Что выведет в консоль данный код?
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {
-
-  },
-  actors: {
-
-  },
-  genres: [],
-  privat: false,
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько вы оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько вы оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+for (let i = 1; i < 5; i++) {
+    console.log(i + 1)
+}
