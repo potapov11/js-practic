2decb38de3505934a7cae7ed9fe3a5cc7069d342
Serezha <volos11223344@yandex.ru>
2022-12-08 10:30:39 +0300

    test js-3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 // Что будет содержаться в переменной time?
 
 let time = '';
-5 > 3 || 2 ? time = 'Day' : time = 'Night' 
+5 > 3 || 2 ? time = 'Day' : time = 'Night';
+
+console.log(time);
+
+// то же самое - 
+
+if ( 5 > 3 || 2) {
+  time  = 'Day'
+}
+else {
+  time = 'Night'
+}
