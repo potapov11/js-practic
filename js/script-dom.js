08b410dfac723b0a405d7a44a0cb9ee4944abc88
Serezha <volos11223344@yandex.ru>
2022-12-12 21:34:58 +0300

    dom-work

--- js/script-dom.js
+++ js/script-dom.js
@@ -12,3 +12,14 @@ console.log(btns);
 
 //!Псевдомассив не подвержен изменениям, поэтому обращаться к элементам необходимо только через запись с индексом
 
+//!Получение элемента по классу
+const circles = document.getElementsByClassName('circle');
+
+//!Метод querySelectorAll
+const hearts = document.querySelectorAll('.heart'); //Получение псевдомассива
+console.log(hearts);
+
+hearts.forEach(function(item) { //Перебор псевдомассива с помощью forEach
+    console.log(item);
+});
+
