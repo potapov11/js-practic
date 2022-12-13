22a6ff147a1590c4008eaf0a92305cd3c1cf1806
Serezha <volos11223344@yandex.ru>
2022-12-13 20:54:47 +0300

    cssin js

--- js/script-dom.js
+++ js/script-dom.js
@@ -1,39 +1,45 @@
 
-'use strict';
+// 'use strict';
 
-const box = document.getElementById('box'); 
+// const box = document.getElementById('box'); 
 
-const btns = document.querySelectorAll('button');
-console.log(btns);
+// const btns = document.querySelectorAll('button');
+// console.log(btns);
 
-for(let i = 1; i < 2; i++) {
-	btns[i].style.backgroundColor = 'gold';
-}
+// // for(let i = 1; i < 2; i++) {
+// // 	btns[i].style.backgroundColor = 'gold';
+// // }
 
 // btns.forEach(function(item){
-// 	item.style.backgroundColor = 'green';
+// 	console.log(btns[item]);
 // });
 
-const circles = document.getElementsByClassName('circle');
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
 
 
-const heart = document.querySelector('.heart');
+// const heart = document.querySelector('.heart');
 
-box.style.backgroundColor = 'blue'; //! Запись стилей css в одну строку 
-box.style.width = '500px';
+// box.style.backgroundColor = 'blue'; //! Запись стилей css в одну строку 
+// box.style.width = '500px';
 
-box.style.cssText = 'background-color: blue; width: 500px'; //! Запись нескольких свойств стилей
+// box.style.cssText = 'background-color: blue; width: 500px'; //! Запись нескольких свойств стилей
 
-btns[1].style.borderRadius = '50%';
+// btns[1].style.borderRadius = '50%';
 
-circles.forEach(function(item) {
-	console.log(item);
-	// item.style.backgroundColor = 'gold';
-});
+// btns.forEach(function(item) {
+// 	console.log(item);
+// 	// item.style.backgroundColor = 'gold';
+// });
 
-btns.forEach(function(item) {
-	console.log(item);
-	// item.style.backgroundColor = 'gold';
-});
 
+const div = document.createElement('div'); //! Создаем элемент -1
+
+div.classList.add('black'); //! Присваиваем ему класс black -2
+
+document.body.append(div); //! Добавляем элемент в конец блока body
+
+document.querySelector('.wrapper').append(div);
+
+document.querySelector('.wrapper').style.display = 'flex';
 
