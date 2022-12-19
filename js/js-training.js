987e268abf9a87a85d25b8a88befd450d094643a
Serezha <volos11223344@yandex.ru>
2022-12-19 21:05:59 +0300

    new lession

--- js/js-training.js
+++ js/js-training.js
@@ -1,24 +1,3 @@
-const objName = {
-	names: [
-		'Женя',
-		"Коля",
-		"Слава",
-		"Марина",
-		"Виталий",
-	]
-};
-
-const namesList = document.querySelector('.names__list');
-namesList.innerHTML = '';
-// const log = console.log;
-
-objName.names.sort();
-// log(objName.names.sort());
-
-objName.names.forEach(function(name){
-	namesList.innerHTML += `
-	<li class="name__item">${name}</li>
-	`;
-});
+const log = console.log;
 
 // log(namesList);
