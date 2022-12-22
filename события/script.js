ffad8841ba8b61a565344e41c0f9e7820d900e22
Serezha <volos11223344@yandex.ru>
2022-12-22 21:44:58 +0300

    new task

--- события/script.js
+++ события/script.js
@@ -1,17 +1,18 @@
-let skills = {
-  "4": "React",
-  "1": "HTML",
-  "2": "CSS",
-  "3": "JavaScript",
-  "5": "NodeJS",
+const btn = document.querySelector('button');
+
+btn.addEventListener('mouseenter', function () {
+  console.log('Click');
+});
+ 
+
+const deleteElement =  function() { //Функция которая удаляет кнопку
+  console.log('Click');
 };
-     
-let mySkills = {};
 
-//! Клонировнаие объекта в другой объект путем перебора for in
+let i = 0;
+  i++;
+  if(i == 2) {
+  btn.removeEventListener('mouseenter', deleteElement);
+  }
 
-for (let key in skills) {
-  mySkills[key] = skills[key];
-}
-
-console.log(mySkills);
+btn.removeEventListener('mouseenter', deleteElement);
