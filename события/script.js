0990c17553580ed2297b39202c728cbb8653fed9
Serezha <volos11223344@yandex.ru>
2022-12-25 19:52:48 +0300

    new commit

--- события/script.js
+++ события/script.js
@@ -21,3 +21,23 @@ console.log(makeNegative(1));
 console.log(makeNegative(-5));
 console.log(makeNegative(0));
 console.log(makeNegative(0.12));
+
+// В этом простом задании вам дается число, и вы должны сделать его отрицательным. Но, может быть, число уже отрицательное?
+
+// Примеры
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+  console.log(makeNegative(1));
+  console.log(makeNegative(-5));
+  console.log(makeNegative(0));
+  console.log(makeNegative(0.12));
