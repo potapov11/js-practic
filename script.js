0e83e27e53a8f9871847eed9d236ab54059018d1
Serezha <volos11223344@yandex.ru>
2023-01-08 22:50:02 +0300

    parenthesis task

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-'use strict';
+//Задача со скобками
+//Верно ли количество открывающих и закрывающих скобочек в строке: ()
 
+let parentes = '(())(())(())';
+
+function getParent(a) {
+  let count = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    if(a[i] == '(') {
+      count++;
+    } else if(a[i] == ')') {
+      count--;
+    }
+    if(count == -1) {
+      return false;
+      break;
+    }
+  }
+  if(count !== 0) {
+    return false;
+} else {
+  return true;
+}
+}
+
+console.log(getParent(parentes));
