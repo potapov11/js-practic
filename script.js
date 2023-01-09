3a85cf7dfce9c0882d0f46ec56d80a6ff5a2e7e2
Serezha <volos11223344@yandex.ru>
2023-01-09 21:35:05 +0300

    indexOf example

--- script.js
+++ script.js
@@ -3,12 +3,10 @@
 const a = [3,9,8,7,5,3,5,2];
 const b = ['hi', 'hello'];
 
-console.log(a.indexOf(8));
-
 // console.log(b.indexOf('Hello'));
 
 // console.table(a);
-// console.log(a.indexOf(2, 4)); //Где 4 это необязательный параметр, указывающий с какой позиции нужно начинать поиск
+console.log(a.indexOf(8)); //Где 4 это необязательный параметр, указывающий с какой позиции нужно начинать поиск
 
 
 
