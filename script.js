c3e831ed2957afce3e4bbe6f9c0cb5e4e30fdccb
Serezha <volos11223344@yandex.ru>
2023-01-09 21:06:11 +0300

    indexOf function

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const a = [9, 8, 7, 5];
+
+
+const a = [3,9,8,7,5,3,5,2];
+const b = ['hi', 'hello'];
 
 console.log(a.indexOf(8));
+
+// console.log(b.indexOf('Hello'));
+
+// console.table(a);
+// console.log(a.indexOf(2, 4)); //Где 4 это необязательный параметр, указывающий с какой позиции нужно начинать поиск
+
+
+
+function indexOfEmul(arr, item, from = 0) { //Принимает два параметра - массив(arr) и значение(item) которое будет искать
+  let result = -1;
+  for (let i = from; i < arr.length; i++) {
+    if(arr[i] === item) {
+      return i;
+    }
+  }
+return result;
+}
+
+console.log(indexOfEmul(a, 8));
