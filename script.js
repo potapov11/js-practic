8d24ff9f6169c055bba08a20a0b7beebc8ce930b
Serezha <volos11223344@yandex.ru>
2023-01-09 18:22:07 +0300

    task par

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //Задача со скобками
 //Верно ли количество открывающих и закрывающих скобочек в строке: ()
 
-let parentes = '(())(())(())';
+const a = ')((()))()((()))((()))';
 
-function getParent(a) {
-  let count = 0;
+let count = 0;
 
-  for (let i = 0; i < a.length; i++) {
-    if(a[i] == '(') {
-      count++;
-    } else if(a[i] == ')') {
-      count--;
-    }
-    if(count == -1) {
-      return false;
-      break;
-    }
+for (let i = 0; i < a.length; i++) {
+  if(a[i] == '(') {
+    count++;
+  } else if(a[i] == ')') {
+    count--;
+  } 
+  if(count == -1) {
+    console.log('megafalse');
+    break;
   }
-  if(count !== 0) {
-    return false;
-} else {
-  return true;
 }
-}
+ if(count !== 0) {
+  console.log(false);
+ } else if(count == -1) {
+  console.log();
+ }
+ else {
+  console.log(true);
+ }
 
-console.log(getParent(parentes));
