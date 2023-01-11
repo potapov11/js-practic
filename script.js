4cc58b2490cf77fe56b9a82c61b0ae31353cc059
Serezha <volos11223344@yandex.ru>
2023-01-11 08:38:48 +0300

    codewars task

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
+const arr = ['hello', 'world', 'this', 'is', 'great'];
 
-const a = [3,9,8,7,5,3,5,2];
-const b = ['hi', 'hello'];
+const smash = words => words.join(' ');
 
-// console.log(b.indexOf('Hello'));
+console.log(smash(arr));
 
-// console.table(a);
-console.log(a.indexOf(8)); //Где 4 это необязательный параметр, указывающий с какой позиции нужно начинать поиск
-
-
-
-function indexOfEmul(arr, item, from = 0) { //Принимает два параметра - массив(arr) и значение(item) которое будет искать
-  let result = -1;
-  for (let i = from; i < arr.length; i++) {
-    if(arr[i] === item) {
-      return i;
-    }
-  }
-return result;
-}
-
-console.log(indexOfEmul(a, 8));
+//Метод join соединяет все элементы массива в строку
